47d7daca665f9f7239b15b3fa394234e1c187ef2
markinveng <markinveng.biz@gmail.com>
2024-11-08 23:47:09 +0900

    デプロイテスト2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,7 @@ export default async function Home() {
       <main>
         <div>This is Homepage</div>
         <div>Coming soon...</div>
-        <div>デプロイテスト</div>
-        {/* <p>{data.contents[0].content}</p> */}
+        <p>{data.contents[0].content}</p>
       </main>
     </>
   );
