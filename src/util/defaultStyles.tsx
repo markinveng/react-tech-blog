4eea1dfd61c1ddda05c272f90b16d9573ce72cc7
markinveng <markinveng.biz@gmail.com>
2024-11-09 20:40:09 +0900

    修正

--- src/util/defaultStyles.tsx
+++ src/util/defaultStyles.tsx
@@ -1,6 +1,6 @@
 export const defaultStyles = {
   //テキストスタイル
   text: "dark:text-white text-black",
-  colorText: "ark:text-cyan-400 text-fuchsia-400",
+  colorText: "dark:text-cyan-400 text-fuchsia-400",
   titleText: ""
 } as const satisfies Record<string, string>;
