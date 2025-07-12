80d073224febb083c1ba3076a27cb88cf00997f6
markinveng <markinveng.biz@gmail.com>
2025-07-12 10:55:25 +0900

    ポインター修正

--- src/components/pointer/Pointer.tsx
+++ src/components/pointer/Pointer.tsx
@@ -5,60 +5,32 @@ import { useEffect, useState } from "react";
 export default function Pointer(): JSX.Element | null {
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const checkScreenSize: () => void = (): void => {
       const breakpoint: number = 768; // タブレット以下のブレークポイント
+      console.log(window.innerWidth > breakpoint);
       setIsVisible(window.innerWidth > breakpoint);
     };
 
-    checkScreenSize(); // 初期チェック
-    window.addEventListener("resize", checkScreenSize);
-
+    checkScreenSize();
     return (): void => {
       window.removeEventListener("resize", checkScreenSize);
     };
   }, []);
 
   useEffect(() => {
+
     const mouseMoveListener: (event: MouseEvent) => void = (event: MouseEvent): void => {
-      const screenWidth: number = window.innerWidth;
-      const screenHeight: number = window.innerHeight;
-
-      // マウスが画面外に出た場合は非表示
-      if (
-        event.clientX < 0 || // 左端
-        event.clientY < 0 || // 上端
-        event.clientX > screenWidth || // 右端
-        event.clientY > screenHeight // 下端
-      ) {
-        setIsVisible(false);
-      } else {
-        // 画面内に戻った場合は再表示
-        setIsVisible(true);
-      }
-
       setMouseX(event.clientX);
       setMouseY(event.clientY);
     };
 
-    const mouseLeaveListener: () => void = (): void => {
-      setIsVisible(false); // マウスがブラウザ外に出たら非表示
-    };
-
-    const mouseEnterListener: () => void = (): void => {
-      setIsVisible(true); // マウスがブラウザ内に戻ったら再表示
-    };
-
     window.addEventListener("mousemove", mouseMoveListener);
-    window.addEventListener("mouseleave", mouseLeaveListener);
-    window.addEventListener("mouseenter", mouseEnterListener);
 
     return (): void => {
       window.removeEventListener("mousemove", mouseMoveListener);
-      window.removeEventListener("mouseleave", mouseLeaveListener);
-      window.removeEventListener("mouseenter", mouseEnterListener);
     };
   }, []);
 
